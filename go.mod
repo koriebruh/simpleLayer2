df1cae0b37debb54882bf668a3c5add68b6fa546
koriebruh <koriebruh@gmail.com>
2025-01-20 21:16:31 +0700

    eheheh

--- go.mod
+++ go.mod
@@ -18,9 +18,14 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/testify v1.10.0 // indirect
 	github.com/supranational/blst v0.3.13 // indirect
-	golang.org/x/crypto v0.22.0 // indirect
-	golang.org/x/sync v0.7.0 // indirect
-	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/crypto v0.32.0 // indirect
+	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250115164207-1a7da9e5054f // indirect
+	google.golang.org/grpc v1.69.4 // indirect
+	google.golang.org/protobuf v1.36.3 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	rsc.io/tmplfunc v0.0.3 // indirect
 )
--- server/main.go
+++ server/main.go
@@ -2,29 +2,106 @@ package main
 
 import (
 	"context"
+	"crypto/ecdsa"
+	"encoding/json"
+	"fmt"
+	"github.com/cockroachdb/errors/grpc/status"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/log"
 	pb "github.com/koriebruh/simpleLayer2/proto/layer2"
+	"google.golang.org/grpc/codes"
+	"math/big"
+	"os"
 	"sync"
 )
 
-type layer2Server struct {
+type Layer2Handler struct {
 	pb.UnimplementedLayer2ServiceServer
 	mu          sync.Mutex
 	transaction []*pb.TransactionRequest
 }
 
-func (l *layer2Server) SubmitTransaction(ctx context.Context, request *pb.TransactionRequest) (*pb.TransactionResponse, error) {
+func (l *Layer2Handler) SubmitTransaction(ctx context.Context, req *pb.TransactionRequest) (*pb.TransactionResponse, error) {
+	log.Info(fmt.Sprintf("Incoming Request %v", req))
+
+	// logic check before we save in temp storage
+	client, err := ethclient.DialContext(ctx, "https://mainnet.infura.io/v3/7108f6b019944d2082df7b667e6b1f4a")
+	if err != nil {
+		return &pb.TransactionResponse{
+			TransactionId: req.TransactionId,
+			Status:        "failed",
+			Message:       "Internal Server Error",
+		}, err
+	}
+
+	pubKey, err := crypto.UnmarshalPubkey([]byte(req.Sender))
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "Invalid sender public key: %v", err)
+	}
+
+	if err = filter(ctx, client, *pubKey, big.NewInt(req.Amount)); err != nil {
+		return nil, err
+	}
+
+	if err = addTxPool(req); err != nil {
+		return nil, err
+	}
+
+	return &pb.TransactionResponse{
+		TransactionId: req.TransactionId,
+		Status:        "success",
+		Message:       "Transaction processed successfully",
+	}, nil
+}
+
+func (l *Layer2Handler) MonitorBatchStatus(req *pb.BatchStatusRequest, stream pb.Layer2Service_MonitorBatchStatusServer) error {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (l *layer2Server) MonitorBatchStatus(ctx context.Context, request *pb.BatchStatusRequest, stream pb.Layer2Service_MonitorBatchStatusClient) error {
+func (l *Layer2Handler) TriggerBatchProcessing(ctx context.Context, req *pb.BatchProcessingRequest) (*pb.BatchProcessingResponse, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (l *layer2Server) TriggerBatchProcessing(ctx context.Context, request *pb.BatchProcessingRequest) (*pb.BatchProcessingResponse, error) {
-	//TODO implement me
-	panic("implement me")
+// filter checking insert User
+func filter(ctx context.Context, c *ethclient.Client, sender ecdsa.PublicKey, valueSend *big.Int) error {
+	// checking amount sender
+	address := crypto.PubkeyToAddress(sender)
+	balanceRN, err := c.BalanceAt(ctx, address, nil)
+	if err != nil {
+		return status.Errorf(codes.Internal, "Failed to retrieve balance: %v", err)
+	}
+
+	// validate
+	if balanceRN.Cmp(valueSend) < 0 {
+		return status.Errorf(codes.InvalidArgument, "Insufficient funds for transaction: %v", err)
+	}
+
+	return nil
+	//// DO CHECK MORE
+	//// DO CHECK MORE
+}
+
+// addTxPool save transaction  the corresponding transaction to the transaction_pool.txt file
+func addTxPool(req *pb.TransactionRequest) error {
+	file, err := os.OpenFile("store/transaction_pool.txt", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		log.Info("Error OpenFile")
+	}
+
+	m, err := json.Marshal(req)
+	if err != nil {
+		return status.Errorf(codes.Internal, "Failed to retrieve balance: %v", err)
+	}
+
+	_, err = file.WriteString(string(m))
+	if err != nil {
+		return status.Errorf(codes.Internal, "Failed to retrieve balance: %v", err)
+	}
+
+	return nil
 }
 
 func main() {
