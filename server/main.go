0cd5aea087f5148c74f51e2fd36eaf5113ef061e
koriebruh <koriebruh@gmail.com>
2025-01-20 22:17:12 +0700

    save

--- server/main.go
+++ server/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"crypto/ecdsa"
 	"encoding/json"
 	"fmt"
 	"github.com/cockroachdb/errors/grpc/status"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/ethereum/go-ethereum/log"
@@ -63,6 +66,9 @@ func (l *Layer2Handler) MonitorBatchStatus(req *pb.BatchStatusRequest, stream pb
 func (l *Layer2Handler) TriggerBatchProcessing(ctx context.Context, req *pb.BatchProcessingRequest) (*pb.BatchProcessingResponse, error) {
 	//TODO implement me
 	panic("implement me")
+
+	// do read file
+
 }
 
 // filter checking insert User
@@ -99,6 +105,102 @@ func addTxPool(req *pb.TransactionRequest) error {
 	_, err = file.WriteString(string(m))
 	if err != nil {
 		return status.Errorf(codes.Internal, "Failed to retrieve balance: %v", err)
+	}
+
+	return nil
+}
+
+// batchInsert for insert
+// private key for signed by the system to represent the sender, and open batch insert to blockchain
+func batchInsert(ctx context.Context, client *ethclient.Client, privateKeyHex string) error {
+	file, err := os.Open("store/transaction_pool.txt")
+	if err != nil {
+		return fmt.Errorf("error opening transaction pool file: %v", err)
+	}
+	defer file.Close()
+
+	// Extract privateKey System
+	privateKey, err := crypto.HexToECDSA(privateKeyHex)
+	if err != nil {
+		return fmt.Errorf("invalid private key: %v", err)
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		return fmt.Errorf("invalid public key")
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	nonce, err := client.PendingNonceAt(ctx, fromAddress)
+	if err != nil {
+		return fmt.Errorf("failed to get nonce: %v", err)
+	}
+
+	chainID, err := client.ChainID(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get chain id: %v", err)
+	}
+
+	// Get suggested gas tip cap (miner tip)
+	gasTipCap, err := client.SuggestGasTipCap(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get gas tip cap: %v", err)
+	}
+
+	// Get suggested
+	gasPrice, err := client.SuggestGasPrice(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get gas fee cap: %v", err)
+	}
+
+	// Calculate fee cap: max(2 * tipCap, gasPrice)
+	gasFeeCap := new(big.Int).Mul(gasTipCap, big.NewInt(2))
+	if gasFeeCap.Cmp(gasPrice) < 0 {
+		gasFeeCap = gasPrice
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		var tx pb.TransactionRequest
+		if err := json.Unmarshal([]byte(line), &tx); err != nil {
+			fmt.Println("Error unmarshaling JSON:", err)
+			continue
+		}
+
+		amount := big.NewInt(tx.Amount)
+		toAddress := common.HexToAddress(tx.Receiver)
+		txData := &types.DynamicFeeTx{
+			ChainID:   chainID,
+			Nonce:     nonce,
+			To:        &toAddress,
+			Value:     amount,
+			Gas:       21000,
+			GasTipCap: gasTipCap, // max priority fee per gas
+			GasFeeCap: gasFeeCap, // max fee per gas
+		}
+
+		transaction := types.NewTx(txData)
+		signer := types.NewLondonSigner(chainID)
+
+		// Sign the transaction
+		signedTx, err := types.SignTx(transaction, signer, privateKey)
+		if err != nil {
+			fmt.Printf("Failed to sign transaction: %v\n", err)
+			continue
+		}
+
+		// Send the signed transaction
+		err = client.SendTransaction(ctx, signedTx)
+		if err != nil {
+			fmt.Printf("Failed to send transaction: %v\n", err)
+			continue
+		}
+
+		fmt.Printf("Transaction sent: ID=%s, Hash=%s\n", tx.TransactionId, signedTx.Hash().Hex())
+		nonce++
 	}
 
 	return nil
